5aca5a7a1b68552a30f959deaadc92634f3025d8
Fyfrel <54896601+Fyfres@users.noreply.github.com>
2021-02-25 10:01:09 +0100

    [Update] Basic installer configured

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require ('electron')
-
+const path =require('path');
+const isDev = require("electron-is-dev");
 require('@electron/remote/main').initialize()
 
 const createWindow = () => {
@@ -11,8 +12,7 @@ const createWindow = () => {
             enableRemoteModule: true
         }
     })
-
-    win.loadURL('http://localhost:3000')
+    win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
 
     // Install React Dev Tools
     const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
